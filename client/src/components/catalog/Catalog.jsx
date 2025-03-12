ddc994989baf7940a9d44c26a84d83a6019c47bd
tinteto <radosttina.dimitrova@gmail.com>
2025-03-12 14:55:33 +0200

    Show notification when catalog is empty

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -9,9 +9,7 @@ const [movies, setMovies] = useState([]);
     useEffect(() => {
     apiService.getAllItems()
     .then(result => {
-        console.log(result);
-        
-       setMovies(result);
+    setMovies(result);
     })
     }, []);
 
@@ -22,7 +20,8 @@ const [movies, setMovies] = useState([]);
     
 {movies.map(movie => <MovieCatalogItem key={movie._id} {...movie} />)}
 
-<h3 className="noItems">No Movies Yet</h3>
+{movies.length === 0 && <h3 className="noItems">No Movies Yet</h3>}
+
 </section>
 </>
     )
