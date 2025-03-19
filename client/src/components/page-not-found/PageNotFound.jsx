313f0f413b5de38533c7bc2a65dc899490b8f47b
tinteto <radosttina.dimitrova@gmail.com>
2025-03-19 13:22:54 +0200

    Page Not Found update

--- client/src/components/page-not-found/PageNotFound.jsx
+++ client/src/components/page-not-found/PageNotFound.jsx
@@ -1,4 +1,4 @@
-//TODO: change colors and eventually add image
+//TODO: change colors and eventually add image - @InsideTheMagic source for the image
 import styles from './PageNotFound.module.css'
 import { Link } from "react-router";
 
@@ -6,10 +6,18 @@ export default function PageNotFound () {
     return (
         <>
     <div className={styles.errorContainer}>
+  
+        <div className={styles.errorImage}>
+        <img src="\images\E2bge_2XwAA7ef9.png" alt="" />
+        </div>
+
+        <div className={styles.errorMessage}>
         <h1>404</h1>
-        <p>Sorry! We are having troubles Finding Nemo! Please try again later or search another adventure!</p>
+        <p>Sorry! We are having troubles <strong>Finding Nemo</strong>! Please try again later or search another adventure!</p>
         <Link className={styles.homeLink} to="/">Go to Homepage</Link>
+        </div>
     </div>
+ 
         </>
     )
 }
