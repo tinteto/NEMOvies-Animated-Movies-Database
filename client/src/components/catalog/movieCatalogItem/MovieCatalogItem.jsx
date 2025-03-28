67aba1fdd2223a72d525aafc577a89e2048ade35
tinteto <radosttina.dimitrova@gmail.com>
2025-03-28 16:44:44 +0200

    Add new features to movie catalog

--- client/src/components/catalog/movieCatalogItem/MovieCatalogItem.jsx
+++ client/src/components/catalog/movieCatalogItem/MovieCatalogItem.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router";
 import styles from './MovieCatalogItem.module.css'
 
 
-export default function MovieCatalogItem({_id, title, description, img, _createdOn}) {
+export default function MovieCatalogItem({_id, title, releaseDate, img}) {
     return (
     <> 
     <div className={styles.movieCard}>
         <img className={styles.imgCard} src={img} alt="MovieImg" />
         <h4 className={styles.title}>{title}</h4>
+        <h4 className={styles.releaseDate}>{releaseDate}</h4>
         <Link className={styles.cardButton} to={`/catalog/${_id}/details`}>Movie Details</Link>
     </div>
 </>
