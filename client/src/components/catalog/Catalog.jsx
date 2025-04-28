ce7ba21d89033f6218617a6a1a7b002e9ed9731c
tinteto <radosttina.dimitrova@gmail.com>
2025-04-28 12:22:06 +0300

    Add pagination

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,10 +1,28 @@
 import MovieCatalogItem from './movieCatalogItem/MovieCatalogItem';
-import { useAllMovies } from '../../apiHooks/movieApiHooks';
 import styles from './Catalog.module.css'
+import { useEffect, useState } from 'react';
 
 
 export default function Catalog() {
-const { movies } = useAllMovies();
+const [movies, setMovies] = useState([]);
+const [page, setPage] = useState(1);
+// const [total, setTotal] = useState(0);
+const pageSize = 4;
+
+useEffect(() => {
+const offset = (page - 1) * pageSize;
+
+fetch(`http://localhost:3030/data/movies?offset=${offset}&pageSize=${pageSize}`)
+.then(response => response.json())
+.then(result => {
+    setMovies(result);
+   // setTotal(total);
+    
+});
+}, [page]);
+
+
+// const totalPages = Math.ceil(total / pageSize);
 
     return(
 <>
@@ -17,6 +35,12 @@ const { movies } = useAllMovies();
    <div className={styles.noMovies}>
     {movies.length === 0 && <h3 className={styles.noMovies}>No Movies yet!</h3>}
    </div>
+
+   <div className={styles.pagination}>
+  <button className="prev" onClick={() => setPage(prev => Math.max(prev - 1, 1))} >Prev</button>
+  <span> Page {page}</span>
+  <button className="next" onClick={() => setPage(prev => prev + 1)} disabled={movies.length === 0}>Next</button>
+</div>
 
 </section>
 </>
